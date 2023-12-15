190cf2954fffcedff69abc172402403e1aed971d
mramu_1996 <mandaloju.ramu@gmail.com>
2023-12-15 17:20:14 -0500

    added basic permissions code

--- app/src/main/java/com/example/applicationcamerax/MainActivity.kt
+++ app/src/main/java/com/example/applicationcamerax/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.example.applicationcamerax
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.applicationcamerax.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // request camera permissions
+        if(allPermissionsGranted()){
+            startCamera()
+        }else requestPermissions()
+
+    }
+
+    private fun allPermissionsGranted() =
+        REQUIRED_PERMISSIONS.all {
+            ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+        }
+
+    private fun takePhoto() {}
+
+    private fun captureVideo() {}
+
+    private fun startCamera() {}
+
+    private fun requestPermissions() {}
+
+    companion object{
+        private const val TAG = "CameraXApplication"
+        private val REQUIRED_PERMISSIONS = mutableListOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO
+        ).apply {
+            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P)
+                add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }.toTypedArray()
     }
 }
