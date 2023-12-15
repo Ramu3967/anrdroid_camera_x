b539478034823119c2b6fd52a58d0c4558854e8a
mramu_1996 <mandaloju.ramu@gmail.com>
2023-12-15 18:02:53 -0500

    request basic permissions and handling the results

--- app/src/main/java/com/example/applicationcamerax/MainActivity.kt
+++ app/src/main/java/com/example/applicationcamerax/MainActivity.kt
@@ -4,6 +4,8 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.example.applicationcamerax.databinding.ActivityMainBinding
@@ -11,15 +13,31 @@ import com.example.applicationcamerax.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+
+    private val activityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()){permissions ->
+        // Handle the permission results
+        var permissionGranted = true
+        permissions.entries.forEach{
+            if (it.key in REQUIRED_PERMISSIONS && it.value == false) permissionGranted =false
+        }
+        if (!permissionGranted)
+            Toast.makeText(baseContext, "Permissions request denied", Toast.LENGTH_SHORT).show()
+        else startCamera()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         // request camera permissions
-        if(allPermissionsGranted()){
-            startCamera()
-        }else requestPermissions()
+        if (allPermissionsGranted()) startCamera()
+        else requestPermissions()
+
+        // setting up the listeners
+        binding.btnImageCapture.setOnClickListener { takePhoto() }
+        binding.btnVideoCapture.setOnClickListener { captureVideo() }
 
     }
 
@@ -34,7 +52,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun startCamera() {}
 
-    private fun requestPermissions() {}
+    private fun requestPermissions() { activityResultLauncher.launch(REQUIRED_PERMISSIONS)}
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
 
     companion object{
         private const val TAG = "CameraXApplication"
