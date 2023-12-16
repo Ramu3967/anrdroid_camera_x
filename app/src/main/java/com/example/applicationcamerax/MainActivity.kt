e26430373a06a1b384fdb5d7c4b3876e057950c0
mramu_1996 <mandaloju.ramu@gmail.com>
2023-12-15 20:26:34 -0500

    add the preview use case

--- app/src/main/java/com/example/applicationcamerax/MainActivity.kt
+++ app/src/main/java/com/example/applicationcamerax/MainActivity.kt
@@ -4,15 +4,30 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.video.Recorder
+import androidx.camera.video.Recording
+import androidx.camera.video.VideoCapture
 import androidx.core.content.ContextCompat
 import com.example.applicationcamerax.databinding.ActivityMainBinding
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    private var imageCapture: ImageCapture? = null
+    private var videoCapture: VideoCapture<Recorder>? = null
+    private var recording: Recording? = null
+
+    private lateinit var cameraExecutor: ExecutorService
 
     private val activityResultLauncher = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()){permissions ->
@@ -21,6 +36,7 @@ class MainActivity : AppCompatActivity() {
         permissions.entries.forEach{
             if (it.key in REQUIRED_PERMISSIONS && it.value == false) permissionGranted =false
         }
+
         if (!permissionGranted)
             Toast.makeText(baseContext, "Permissions request denied", Toast.LENGTH_SHORT).show()
         else startCamera()
@@ -39,6 +55,7 @@ class MainActivity : AppCompatActivity() {
         binding.btnImageCapture.setOnClickListener { takePhoto() }
         binding.btnVideoCapture.setOnClickListener { captureVideo() }
 
+        cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
     private fun allPermissionsGranted() =
@@ -50,12 +67,32 @@ class MainActivity : AppCompatActivity() {
 
     private fun captureVideo() {}
 
-    private fun startCamera() {}
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener({
+            // used to bind the lifecycle of the camera to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also { it.setSurfaceProvider(binding.viewFinder.surfaceProvider) }
+            // select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            kotlin.runCatching {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+                // bind use cases to camera
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            }.onFailure { Log.e(TAG, "startCamera: Use case binding failed $it ", ) }
+
+        },ContextCompat.getMainExecutor(this))
+    }
 
     private fun requestPermissions() { activityResultLauncher.launch(REQUIRED_PERMISSIONS)}
 
     override fun onDestroy() {
         super.onDestroy()
+        cameraExecutor.shutdown()
     }
 
     companion object{
